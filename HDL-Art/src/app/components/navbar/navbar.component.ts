303ada32d9f8eabab60a3393cf77666bd68ccf7a
Carlos Ché <3013216470101@ingenieria.usac.edu.gt>
2024-07-02 16:23:08 -0400

    PC Menu View
    Menu Design for PC and laptop size

---
+++ HDL-Art/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  
+  currentUrl: string = '';
+
+  constructor(private router: Router ) { }
+
+  ngOnInit(): void {
+    this.currentUrl = this.router.url;
+  }
+}
